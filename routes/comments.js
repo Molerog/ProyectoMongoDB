d7cce5f7c1cfc2f12e4a1ef746830a19ad32b2ec
Molerog <102958319+Molerog@users.noreply.github.com>
2022-06-07 13:32:03 +0200

    apurando errores

--- routes/comments.js
+++ routes/comments.js
@@ -7,7 +7,7 @@ const { uploadCommentImages } = require('../middleware/multer');
 router.post('/',authentication,uploadCommentImages.single('imageComment'),CommentController.create);
 router.get('/', CommentController.getAll);
 router.delete('/id/:_id',authentication,isAuthorComment,CommentController.delete);
-router.put('/id/:_id',authentication,uploadCommentImages.single('imageComment'),isAuthorComment,CommentController.update);
+router.put('/id/:_id',authentication,isAuthorComment,uploadCommentImages.single('imageComment'),CommentController.update);
 router.put('/likes/:_id', authentication,CommentController.like);
 router.put('/removelikes/:_id',authentication,CommentController.removeLike);
 
